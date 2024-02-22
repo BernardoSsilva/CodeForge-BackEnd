77844265978ffdae1128a77cb3fd9ae091b3e7e1
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-22 17:05:18 -0300

    user entity unit test created

---
+++ src/app/entities/__test__/user.entity.unit-test.spec.ts
@@ -0,0 +1,20 @@
+import { UserEntity } from "../user.entity"
+
+describe("User entity unit tests", () =>{
+    it("Should create a new user entity",() =>{
+        const newUser = new UserEntity({
+            userName:"test Name",
+            userEmail:"test@email",
+            userLogin:"testLogin",
+            userPassword:"testPassword",
+            createdAt:new Date()
+        })
+
+        expect(newUser.id).toBeDefined();
+        expect(newUser.userName).toEqual("test Name");
+        expect(newUser.userEmail).toEqual("test@email");
+        expect(newUser.userLogin).toEqual("testLogin");
+        expect(newUser.userPassword).toEqual("testPassword");
+        expect(newUser.createdAt).toBeInstanceOf(Date);
+    })
+})
