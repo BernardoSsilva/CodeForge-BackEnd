d4808af068b674ff08938d06a622bda55e48309b
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-22 17:20:05 -0300

    created user repository

---
+++ src/app/repositories/user.repository.ts
@@ -0,0 +1,12 @@
+import { UserEntity } from '../entities/user.entity';
+
+export abstract class UserRepository {
+  abstract findAllUsers(): Promise<UserEntity[]>;
+  abstract findUserById(id: string): Promise<UserEntity>;
+  abstract registerUser(user: UserEntity): Promise<void>;
+
+  abstract updateUser(user: UserEntity): Promise<void>;
+  abstract deleteUser(id: string): Promise<void>;
+  abstract getByEmail(email: string): Promise<UserEntity>;
+  abstract getByLogin(login: string): Promise<UserEntity>;
+}
