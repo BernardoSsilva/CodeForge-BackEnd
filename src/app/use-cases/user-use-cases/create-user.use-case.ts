6775452e5feb9664bb8a0f7b7d7fd64572410dbb
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-22 19:44:11 -0300

    created register user use case

---
+++ src/app/use-cases/user-use-cases/create-user.use-case.ts
@@ -0,0 +1,10 @@
+import { UserEntity } from 'src/app/entities/user.entity';
+import { UserRepository } from '../../../app/repositories/user.repository';
+
+export class CreateUserUseCase {
+  constructor(private userRepository: UserRepository) {}
+    async execute(user:UserEntity){
+        await this.userRepository.registerUser(user)
+    }
+
+}
