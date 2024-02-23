832a2191a5a99314022cbd711d85d4c10bfcfa5e
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-23 14:28:41 -0300

    created update comment use case unit tests

--- src/app/use-cases/comment-use-cases/__tests__/get-comment-by-id-use-case.unit-test.spec.ts
+++ src/app/use-cases/comment-use-cases/__tests__/get-comment-by-id-use-case.unit-test.spec.ts
@@ -1,30 +1,28 @@
-import { CommentsInMemoryRepository } from "../../../../../test/helpers/comments-in-memory-repository";
+import { CommentsInMemoryRepository } from '../../../../../test/helpers/comments-in-memory-repository';
 import { CommentEntity } from '../../../../app/entities/comment.entity';
-import { GetCommentByIdUseCase } from "../get-comment-by-id.use-case";
+import { GetCommentByIdUseCase } from '../get-comment-by-id.use-case';
 
-describe("Get all users use cases unit tests", () =>{
-    const commentRepository = new CommentsInMemoryRepository()
-    const getCommentById = new GetCommentByIdUseCase(commentRepository);
+describe('Get all users use cases unit tests', () => {
+  const commentRepository = new CommentsInMemoryRepository();
+  const getCommentById = new GetCommentByIdUseCase(commentRepository);
 
-    it("Should throw an error if has no comments", () =>{
-        expect(async () => await getCommentById.execute("")).rejects.toThrow()
+  it('Should throw an error if has no comments', () => {
+    expect(async () => await getCommentById.execute('')).rejects.toThrow();
+  });
 
-    })
+  it('Should be able to find all comments', async () => {
+    const newComment = new CommentEntity({
+      content: 'testContent',
+      createdAt: new Date(),
+      postId: 'testId',
+      userId: 'testId',
+      tittle: 'testTittle',
+    });
 
-    it("Should be able to find all comments",async () =>{
-        const newComment = new CommentEntity({
-            content: "testContent",
-            createdAt: new Date(),
-            postId: "testId",
-            userId: "testId",
-            tittle: "testTittle"
-        })
+    await commentRepository.postComment(newComment);
 
-        await commentRepository.postComment(newComment)
-
-        const result = await getCommentById.execute(newComment.id)
-        expect(result).toBeDefined       
-        expect(result).toEqual(commentRepository.comments[0])
-
-    })
-})
+    const result = await getCommentById.execute(newComment.id);
+    expect(result).toBeDefined;
+    expect(result).toEqual(commentRepository.comments[0]);
+  });
+});
---
+++ src/app/use-cases/comment-use-cases/__tests__/update-comment-use-case.unit-test.spec.ts
@@ -0,0 +1,67 @@
+import { CommentsInMemoryRepository } from '../../../../../test/helpers/comments-in-memory-repository';
+import { CommentEntity } from '../../../../app/entities/comment.entity';
+import { UpdateCommentUseCase } from '../update-comment.use-case';
+
+describe('Update post use case unit tests', () => {
+  const commentRepository = new CommentsInMemoryRepository();
+  const updateComment = new UpdateCommentUseCase(commentRepository);
+
+  it('Should throw an error if post not found', () => {
+    const newComment = new CommentEntity({
+      content: 'testContent',
+      createdAt: new Date(),
+      postId: 'testId',
+      userId: 'testId',
+      tittle: 'testTittle',
+    });
+
+    expect(
+      async () => await updateComment.execute(newComment, ''),
+    ).rejects.toThrow();
+  });
+
+  it('Should throw an error if not has a edit body', () => {
+    const newComment = new CommentEntity({
+      content: 'testContent',
+      createdAt: new Date(),
+      postId: 'testId',
+      userId: 'testId',
+      tittle: 'testTittle',
+    });
+
+    expect(
+      async () => await updateComment.execute(null, newComment.id),
+    ).rejects.toThrow();
+  });
+
+  it('Should be able to update a post', async () => {
+    const firstComment = new CommentEntity({
+      content: 'testContent',
+      createdAt: new Date(),
+      postId: 'testId',
+      userId: 'testId',
+      tittle: 'testTittle',
+    });
+
+    commentRepository.comments = [firstComment];
+
+    expect(commentRepository.comments[0].id).toEqual(firstComment.id);
+    const newComment = new CommentEntity({
+      content: 'testContent',
+      createdAt: new Date(),
+      postId: 'testId',
+      userId: 'testId',
+      tittle: 'testTittle',
+    });
+
+    newComment.id = firstComment.id;
+
+    await updateComment.execute(newComment, newComment.id);
+
+    expect(commentRepository.comments).toHaveLength(1);
+    expect(commentRepository.comments[0].tittle).toEqual(newComment.tittle);
+    expect(commentRepository.comments[0].userId).toEqual(newComment.userId);
+    expect(commentRepository.comments[0].content).toEqual(newComment.content);
+    expect(commentRepository.comments[0].postId).toEqual(newComment.postId);
+  });
+});
