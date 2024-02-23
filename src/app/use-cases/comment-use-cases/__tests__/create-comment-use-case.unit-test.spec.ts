112ea626dac056e864199dd35fa60d5ecf39799e
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-23 13:52:25 -0300

    created create comment use case unit tests

--- src/app/use-cases/comment-use-cases/__tests__/create-comment-use-case.unit-test.spec.ts
+++ src/app/use-cases/comment-use-cases/__tests__/create-comment-use-case.unit-test.spec.ts
@@ -1,3 +1,32 @@
+import { CommentRepository } from 'src/app/repositories/comment.repository';
+import { CommentsInMemoryRepository } from '../../../../../test/helpers/comments-in-memory-repository';
+import { CreateCommentUseCase } from '../create-comment.use-case';
+import { CommentEntity } from '../../../../app/entities/comment.entity';
 describe("Create a new comment use case unit tests",() =>{
-    
+
+    const commentRepository = new CommentsInMemoryRepository()
+    const createComment = new CreateCommentUseCase(commentRepository)
+    it("Should throw error if send a empty comment",() =>{
+        expect(async () => await createComment.execute(null)).rejects.toThrow();
+    })
+
+    it("Should be able to create a new comment", async () =>{
+        const newComment = new CommentEntity({
+            content: "testContent",
+            createdAt: new Date(),
+            postId: "testId",
+            userId: "testId",
+            tittle: "testTittle"
+        })
+
+        await createComment.execute(newComment)
+
+        expect(commentRepository.comments[0]).toBeDefined()
+        expect(commentRepository.comments[0].id).toEqual(newComment.id)
+        expect(commentRepository.comments[0].content).toEqual(newComment.content)
+        expect(commentRepository.comments[0].postId).toEqual(newComment.postId)
+        expect(commentRepository.comments[0].userId).toEqual(newComment.userId)
+        expect(commentRepository.comments[0].tittle).toEqual(newComment.tittle)
+        expect(commentRepository.comments[0].createdAt).toBeInstanceOf(Date)
+    })
 })
