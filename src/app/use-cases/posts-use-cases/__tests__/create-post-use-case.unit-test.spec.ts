9cbf60aa1e915f3ddc4e98af790d6dfb2aa96ecf
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-23 10:21:03 -0300

    created create post use case unit tests

--- src/app/use-cases/posts-use-cases/__tests__/create-post-use-case.unit-test.spec.ts
+++ src/app/use-cases/posts-use-cases/__tests__/create-post-use-case.unit-test.spec.ts
@@ -1,8 +1,29 @@
-import { PostsInMemoryRepository } from "../../../../../test/helpers/post-in-memory.repository";
-import { CreatePostUseCase } from "../create-post.use-case";
+import { PostEntity } from '../../../../app/entities/post.entity';
+import { PostsInMemoryRepository } from '../../../../../test/helpers/post-in-memory.repository';
+import { CreatePostUseCase } from '../create-post.use-case';
 
+describe('Create Post use case unit tests', () => {
+  const repository = new PostsInMemoryRepository();
+  const createPost = new CreatePostUseCase(repository);
 
-describe("Create Post use case unit tests",() =>{
-    const repository = new PostsInMemoryRepository();
-    const createPost = new CreatePostUseCase(repository)
-})
+  it("Should throw error if don't have sent post", () => {
+    expect(() => createPost.execute(null)).rejects.toThrow();
+  });
+
+  it('Should be able to create a new post ', async () => {
+    const newPost = new PostEntity({
+      comments: 0,
+      content: 'test content',
+      createdAt: new Date(),
+      likes: 0,
+      tags:["test tag1", "test tag 2"],
+      tittle:"testTitle",
+      userId:"testId"
+    });
+
+    await createPost.execute(newPost)
+
+    expect(repository.posts).toHaveLength(1)
+    expect(repository.posts[0].id).toEqual(newPost.id)
+  });
+});
