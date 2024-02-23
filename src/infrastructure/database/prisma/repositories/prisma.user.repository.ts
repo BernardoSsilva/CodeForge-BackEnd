27501ba9436a9ede8876163a17456d57a4f0787c
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-23 16:23:15 -0300

    created create user use case on prisma user repository

--- src/infrastructure/database/prisma/repositories/prisma.user.repository.ts
+++ src/infrastructure/database/prisma/repositories/prisma.user.repository.ts
@@ -1,43 +1,54 @@
-import { Injectable } from "@nestjs/common";
-import { UserRepository } from "../../../../app/repositories/user.repository";
-import { UserEntity } from "src/app/entities/user.entity";
-import { PrismaService } from "../prisma.service";
-import { UserMapper } from "../mappers/user.mapper";
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '../../../../app/repositories/user.repository';
+import { UserEntity } from 'src/app/entities/user.entity';
+import { PrismaService } from '../prisma.service';
+import { UserMapper } from '../mappers/user.mapper';
 
 @Injectable()
-export class PrismaUserRepository implements UserRepository{
-
-    constructor(private prisma: PrismaService){}
-    async findAllUsers(): Promise<UserEntity[]> {
-        try{
-            const result = await this.prisma.user.findMany()
-            return result.map(user => UserMapper.toDomain(user))
-        }
-        catch{
-            throw new Error
-        }
+export class PrismaUserRepository implements UserRepository {
+  constructor(private prisma: PrismaService) {}
+  async findAllUsers(): Promise<UserEntity[]> {
+    try {
+      const result = await this.prisma.user.findMany();
+      return result.map((user) => UserMapper.toDomain(user));
+    } catch {
+      throw new Error();
     }
-    async findUserById(id: string): Promise<UserEntity> {
-        try{
-            const result = await this.prisma.user.findUnique({where:{userId:id}})
-            return UserMapper.toDomain(result)
-        }catch{
-            throw new Error
-        }
+  }
+  async findUserById(id: string): Promise<UserEntity> {
+    try {
+      const result = await this.prisma.user.findUnique({
+        where: { userId: id },
+      });
+      return UserMapper.toDomain(result);
+    } catch {
+      throw new Error();
     }
-    registerUser(user: UserEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+  }
+  async registerUser(user: UserEntity): Promise<void> {
+    try {
+      await this.prisma.user.create({
+        data: {
+          userEmail: user.userEmail,
+          userLogin: user.userLogin,
+          userName: user.userName,
+          userPassword: user.userPassword,
+        },
+      });
+    } catch {
+      throw new Error();
     }
-    updateUser(user: Partial<UserEntity>, userId: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    deleteUser(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    getByEmail(email: string): Promise<UserEntity> {
-        throw new Error("Method not implemented.");
-    }
-    getByLogin(login: string): Promise<UserEntity> {
-        throw new Error("Method not implemented.");
-    }
+  }
+  updateUser(user: Partial<UserEntity>, userId: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  deleteUser(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  getByEmail(email: string): Promise<UserEntity> {
+    throw new Error('Method not implemented.');
+  }
+  getByLogin(login: string): Promise<UserEntity> {
+    throw new Error('Method not implemented.');
+  }
 }
