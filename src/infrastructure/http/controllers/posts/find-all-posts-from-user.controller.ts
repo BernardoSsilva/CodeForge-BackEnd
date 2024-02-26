344c5d788cd085a437ed494a81878af7294fe631
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-26 17:40:31 -0300

    created auth guard to posts routes

--- src/infrastructure/http/controllers/posts/find-all-posts-from-user.controller.ts
+++ src/infrastructure/http/controllers/posts/find-all-posts-from-user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { FindAllPostsFromUserUseCase } from 'src/app/use-cases/posts-use-cases/find-all-posts-from-user.use-case';
+import { AuthGuard } from 'src/infrastructure/auth/auth.guard';
 
 @Controller('post/all')
 export class FindAllPostsFromUserController {
   constructor(private findAllPostsFromUser: FindAllPostsFromUserUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Get('/:userId')
   async findAllPostsFromUserExecute(@Param('userId') userId: string) {
     return await this.findAllPostsFromUser.execute(userId);
--- src/infrastructure/http/controllers/posts/update-post.controller.ts
+++ src/infrastructure/http/controllers/posts/update-post.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Param, Patch, UseGuards } from '@nestjs/common';
 import { UpdatePostUseCase } from 'src/app/use-cases/posts-use-cases/update-post.use-case';
 import { UpdatePostDto } from './dtos/update.post.dto';
+import { AuthGuard } from 'src/infrastructure/auth/auth.guard';
 
 @Controller('/post/update')
 export class UpdatePostController {
   constructor(private updatePost: UpdatePostUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Patch(':postId')
   async executeUpdate(
     @Param('postId') postId: string,
