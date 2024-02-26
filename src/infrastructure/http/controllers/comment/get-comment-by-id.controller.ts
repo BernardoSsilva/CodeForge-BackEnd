bc2450dd0e93c1124088275a9d59282599b8cc19
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-26 17:11:16 -0300

    created protection to comments routes

--- src/infrastructure/http/controllers/comment/get-comment-by-id.controller.ts
+++ src/infrastructure/http/controllers/comment/get-comment-by-id.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { GetCommentByIdUseCase } from 'src/app/use-cases/comment-use-cases/get-comment-by-id.use-case';
+import { AuthGuard } from 'src/infrastructure/auth/auth.guard';
 
 @Controller('/comment/')
 export class GetCommentByIdController {
   constructor(private getCommentById: GetCommentByIdUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Get('/:commentId')
   async executeSearch(@Param('commentId') commentId: string) {
     const result = await this.getCommentById.execute(commentId);
