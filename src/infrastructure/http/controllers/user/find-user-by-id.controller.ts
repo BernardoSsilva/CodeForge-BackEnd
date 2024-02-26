29f4f0684212765c3ce554579abadaaccc98e591
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-02-26 17:09:32 -0300

    protected user routes

--- src/infrastructure/http/controllers/user/find-user-by-id.controller.ts
+++ src/infrastructure/http/controllers/user/find-user-by-id.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { FindUserByIdUseCase } from 'src/app/use-cases/user-use-cases/find-user-by-id.use-case';
 import { UserPresenter } from './presenters/user.presenter';
+import { AuthGuard } from 'src/infrastructure/auth/auth.guard';
 
 @Controller('/user')
 export class FindUserByIdController {
   constructor(private findUserById: FindUserByIdUseCase) {}
 
+  @UseGuards(AuthGuard)
   @Get('/:id')
   async findUserByIdExecute(@Param('id') id: string) {
     const user = await this.findUserById.execute(id);
